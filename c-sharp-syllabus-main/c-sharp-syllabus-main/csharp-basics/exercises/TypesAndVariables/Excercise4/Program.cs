02b6c77b78203fb0f3209b6b00220aba7694ef7c
KKuzmane <kristinekuzmane@gmail.com>
2021-11-24 16:34:42 +0200

    Excercise 4

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Excercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name;
+string lastName;
+int yearOfBirth;
+
+name = "Kristine";
+lastName = "Kuzmane";
+yearOfBirth = 1995;
+
+
+Console.WriteLine("My name is " + name + " " + lastName + " and I was born in " + yearOfBirth + ".");
+Console.WriteLine(name);
+Console.WriteLine(yearOfBirth);
+Console.ReadKey();
