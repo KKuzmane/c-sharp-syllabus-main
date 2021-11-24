7a5b06cc90320f364e3500d0cf570df608514d07
KKuzmane <kristinekuzmane@gmail.com>
2021-11-24 17:43:49 +0200

    Exercise 5

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Excercise 5/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Id";
+string name2 = "Class";
+string name3 = "Teacher";
+string firstClass = "Math";  
+string secondClass = "Latvian";  
+string thirdClass = "Chemestry"; 
+string forthClass = "Sports";  
+string fifthClass = "History"; 
+string sixtClass = "Music";
+string seventhClass = "English"; 
+string firstTeacher = "R.Sarkovska";  
+string secondTeacher = "I.Strelkova"; 
+string thirdTeacher = "A.Eskina";
+string forthTeacher = "A.Krama";  
+string fifthTeacher = "Z.Otersone";
+string sixtTeacher = "J.Rijnieks";
+string seventhTeacher = "D.Satrovska";
+
+Console.WriteLine("+-----------------------------+");
+Console.WriteLine("| " + name + "| " + name2 + "     | " + name3 + "     |");
+Console.WriteLine("+-----------------------------+");
+Console.WriteLine("| " + "1" + " | " + firstClass + "      | " + firstTeacher + " |");
+Console.WriteLine("| " + "2" + " | " + secondClass + "   | " + secondTeacher + " |");
+Console.WriteLine("| " + "3" + " | " + thirdClass + " | " + thirdTeacher + "    |");
+Console.WriteLine("| " + "4" + " | " + forthClass + "    | " + forthTeacher + "     |");
+Console.WriteLine("| " + "5" + " | " + fifthClass + "   | " + fifthTeacher + "  |");
+Console.WriteLine("| " + "6" + " | " + sixtClass + "     | " + sixtTeacher + "  |");
+Console.WriteLine("| " + "7" + " | " + seventhClass + "   | " + seventhTeacher + " |");
+Console.WriteLine("+-----------------------------+");
+Console.ReadKey();
--- c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Excercise4/Program.cs
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Excercise4/Program.cs
@@ -9,6 +9,4 @@ yearOfBirth = 1995;
 
 
 Console.WriteLine("My name is " + name + " " + lastName + " and I was born in " + yearOfBirth + ".");
-Console.WriteLine(name);
-Console.WriteLine(yearOfBirth);
 Console.ReadKey();
