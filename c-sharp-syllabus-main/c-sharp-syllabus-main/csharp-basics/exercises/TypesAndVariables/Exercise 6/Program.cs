ebf3ab75055951e4b42ab25e316282d2f2cb0eee
KKuzmane <kristinekuzmane@gmail.com>
2021-11-24 17:57:03 +0200

    Exercise 6

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise 6/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your first integer number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your second integer number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your third integer number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = firstNumber + secondNumber + thirdNumber;
+
+Console.WriteLine("Sum of your numbers is: " + sum + ".");
