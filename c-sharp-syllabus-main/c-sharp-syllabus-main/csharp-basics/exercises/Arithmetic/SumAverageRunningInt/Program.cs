e887661ab5e51d3bf79f36bbf90f12783d5ebf43
KKuzmane <kristinekuzmane@gmail.com>
2021-11-25 12:39:01 +0200

    Task SumAverageRunningInt update

--- c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
@@ -7,18 +7,18 @@ namespace SumAverageRunningInt
         static void Main(string[] args)
         {
             double sum = 0;
-            double average;
+            double average = 0;
             const int lowerBound = 1;
             const int upperBound = 100;
 
             for (var number = lowerBound; number <= upperBound; ++number) 
             {
                 sum += number;
-                average = Math.Round(sum / number, 2);
-
-                Console.WriteLine("The average is " + average);
-                Console.WriteLine("The sum of " + lowerBound + " to " + upperBound + " is " + sum);
+                average = Math.Round((sum / number), 2);
+ 
             }
+            Console.WriteLine("The average is " + average);
+            Console.WriteLine("The sum of " + lowerBound + " to " + upperBound + " is " + sum);
         }
     }
 }
