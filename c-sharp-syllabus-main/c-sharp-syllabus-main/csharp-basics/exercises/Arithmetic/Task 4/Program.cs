40148264dc2479f9a454f310c968741d6862291c
KKuzmane <kristinekuzmane@gmail.com>
2021-11-25 12:35:21 +0200

    Task 4

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Task 4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int result = 1;
+const int lowerBound = 1;
+const int upperBound = 10;
+
+for (var number = lowerBound; number <= upperBound; ++number)
+{
+    result *= number;
+}
+Console.WriteLine("The result is " + result);
