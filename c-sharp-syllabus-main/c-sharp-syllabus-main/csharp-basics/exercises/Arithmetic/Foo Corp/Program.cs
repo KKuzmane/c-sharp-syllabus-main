9243ef45612e46f869e87ff3a420251beab66166
KKuzmane <kristinekuzmane@gmail.com>
2021-11-26 12:48:50 +0200

    Task 8

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Foo Corp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foo_Corp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double hourlySalary = 8.00;
+            double overtimeMultiplyer = 1.5;
+            double requiredHours = 40;
+            double maxHoursPerWeek = 60;
+            Console.WriteLine("Enter yor hours here: ");
+            string workedHoursInput = Console.ReadLine();
+            double workedHours = Convert.ToDouble(workedHoursInput);
+            double employeesSalary;
+            if (workedHours > maxHoursPerWeek)
+            {
+                workedHours = maxHoursPerWeek;
+                Console.WriteLine("You have worked more then we allow, so we will count only 60 hours in your paycheck.");
+            }
+            double overtimeHours = workedHours - requiredHours;
+
+            employeesSalary = Math.Round((requiredHours * hourlySalary) + (overtimeHours * (hourlySalary * overtimeMultiplyer)), 2);
+
+            Console.WriteLine("This week you have worked " + workedHours + " hours and there where " + overtimeHours + " overtime hours, so your salary this week is " +
+                employeesSalary + " dollars.");
+            Console.ReadKey();
+        }
+    }
+}
