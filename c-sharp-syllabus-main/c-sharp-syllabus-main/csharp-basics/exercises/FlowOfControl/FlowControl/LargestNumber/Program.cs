5c41713dff099656ef14de79ecbc06758b904ef7
KKuzmane <kristinekuzmane@gmail.com>
2021-11-29 13:57:45 +0200

    Deleted comment and fixed spaces.

--- c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -4,7 +4,6 @@ namespace LargestNumber
 {
     class Program
     {
-        //TODO: Write a C# program to to find the largest of three numbers.
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
@@ -19,40 +18,34 @@ namespace LargestNumber
             var insertedInput3 = Console.ReadLine();
             int input3 = Convert.ToInt32(insertedInput3);
 
-           if (input1 > input2 && input1 > input3)
+            if (input1 > input2 && input1 > input3)
             {
                 Console.WriteLine("Biggest number is " + input1);
             }
-           else if( input2 > input1 && input2 > input3)
+            else if( input2 > input1 && input2 > input3)
             {
                 Console.WriteLine("Biggest number is " + input2);
             }
-           else if (input3> input1 && input3 > input2)
+            else if (input3> input1 && input3 > input2)
             {
                 Console.WriteLine("Biggest number is " + input3);
             }
-           else if (input1 == input2 && input1 > input3)
+            else if (input1 == input2 && input1 > input3)
             {
                 Console.WriteLine("Biggest number is " + input1);
             }
-           else if (input1 == input3 && input1 > input2)
+            else if (input1 == input3 && input1 > input2)
             {
                 Console.WriteLine("Biggest number is " + input1);
             }
-           else if (input2 == input3 && input2 > input1)
+            else if (input2 == input3 && input2 > input1)
             {
                 Console.WriteLine("Biggest number is " + input3);
             }
-           else
+            else
             {
-                Console.WriteLine("Biggest number is " + input1); //Returns a value if all tree numbers are equal
+                Console.WriteLine("Biggest number is " + input1); //Returns a value if all three numbers are equal
             }
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
             Console.ReadKey();
         }
     }
