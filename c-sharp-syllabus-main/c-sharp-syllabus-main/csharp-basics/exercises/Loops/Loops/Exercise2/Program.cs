b49d0ded0762976e97f0a6d165cf3f6f9e8e8e3c
KKuzmane <kristinekuzmane@gmail.com>
2021-12-03 10:52:58 +0200

    Task 2

--- c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
@@ -6,17 +6,20 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            int i, n;
+            int i;
+            int n;
+            int answer = 1;
             
             Console.WriteLine("Input number of terms : ");
             n = Convert.ToInt32(Console.ReadLine());
-            /*
-            todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.Pow()
-            */
-            for (i = 0; i < n; i++)
+            Console.WriteLine("Input number you want to multiply : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            
+            for (int j = 1; j <= n; j++)
             {
+                answer *= i;
             }
-
+            Console.WriteLine($"{i} has been multiplyed {n} times. Resoult is: {answer}");
             Console.ReadKey();
         }
     }
