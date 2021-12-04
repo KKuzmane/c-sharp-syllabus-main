9723c34adb1cd6e093bcc44eb5111ade9f697a14
KKuzmane <kristinekuzmane@gmail.com>
2021-12-04 12:54:14 +0200

    Task 8

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/AsciiFigure/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiFigure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of figure: ");
+            int inputSize = int.Parse(Console.ReadLine());
+            string s = "*";
+            string c = "/";
+            string d = @"\";
+            int size = 0;
+
+            for (int j = 1; j < inputSize; j++)
+            {
+                size += 8;
+            }
+
+            for (int i = 0; i < inputSize; i++)
+            {
+                int a = i * 8;
+                int h = (size - a) / 2;
+                for(int t = 0; t < h; t++)
+                {
+                    Console.Write(c);
+                }
+                for (int v = 0; v < a; v++)
+                {
+                    Console.Write(s);
+                }
+                for (int t = 0; t < h; t++)
+                {
+                    Console.Write(d);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
