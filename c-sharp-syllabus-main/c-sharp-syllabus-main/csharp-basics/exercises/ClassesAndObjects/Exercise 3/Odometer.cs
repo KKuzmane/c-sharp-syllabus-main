1f1a5226590493b87ceb848687f7840c9a9c0a7d
KKuzmane <kristinekuzmane@gmail.com>
2021-12-06 21:40:15 +0200

    Task 3 unfinished

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise 3/Odometer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    internal class Odometer
+    {
+        public int _mileage;
+
+        public Odometer(int currentMileage)
+        {
+            this._mileage = currentMileage;
+        }
+        public int StartMileage()
+        {
+            Console.WriteLine("Enter yout current mileage in km: ");
+            int currentMileage = int.Parse(Console.ReadLine());
+            this._mileage = currentMileage;
+            return currentMileage;
+        }
+        public void Increment(int currentMileage)
+        {
+            if (currentMileage < 999999)
+            {
+                currentMileage++;
+            }
+            else
+            {
+                currentMileage = 0;
+            }
+            this._mileage = currentMileage;
+        }
+        public void FuelDecrement(int currentMileage, FuelGauge carFuel)
+        {
+            if(currentMileage % 10 == 0)
+            {
+                carFuel.Decrement(carFuel._currentFuel);
+            }
+        }
+        public int ReturnsCurrentMileage()
+        {
+            return this._mileage;
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise 3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    internal class FuelGauge
+    {
+        public double _currentFuel;
+        public FuelGauge(double currentFuel)
+        {
+            this._currentFuel = currentFuel;
+        }
+        public double StartFuel()
+            {
+            Console.WriteLine("Enter yout current fuel amount in liters");
+            double currentFuel = double.Parse(Console.ReadLine());
+            this._currentFuel = currentFuel;
+            return currentFuel;
+            }
+        public void Increment(double currentFuel)
+        {
+            if (currentFuel < 70)
+            {
+                this._currentFuel++;
+            }
+            else
+            {
+                Console.WriteLine("Gas tank is full. You must drive!");
+                this._currentFuel = 70;
+            }
+            //this._currentFuel = currentFuel;
+        }
+        public void Decrement(double currentFuel)
+        {
+            if (currentFuel > 0)
+            {
+                currentFuel--;
+            }
+            else
+            {
+                currentFuel = 0;
+            }
+            this._currentFuel = currentFuel;
+        }
+        public double ReturnsCurrentFuel()
+        {
+            return this._currentFuel;
+        }
+        static void Main(string[] args)
+        {
+            FuelGauge fuelCar1 = new FuelGauge(0);
+            double currentFuelCar1 = fuelCar1.StartFuel();
+            Odometer mileageCar1 = new Odometer(0);
+            int currentMileageCar1 = mileageCar1.StartMileage();
+
+                fuelCar1.Increment(currentFuelCar1);
+
+            Console.WriteLine(fuelCar1._currentFuel);
+
+            while(fuelCar1._currentFuel > 0)
+            {
+                mileageCar1.Increment(mileageCar1._mileage);
+                fuelCar1.Decrement(fuelCar1._currentFuel);
+
+                Console.WriteLine($"Mileage: {mileageCar1._mileage}");
+                Console.WriteLine($"Fuel level: {fuelCar1.ReturnsCurrentFuel()}");
+                Console.WriteLine("_________________________");
+            }
+            Console.ReadKey();
+        }
+    }
+}
