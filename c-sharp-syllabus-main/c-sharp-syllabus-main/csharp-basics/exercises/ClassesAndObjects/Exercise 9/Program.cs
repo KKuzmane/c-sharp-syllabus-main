600d15b55134db2d4981778d1cc7456bfa35719c
KKuzmane <kristinekuzmane@gmail.com>
2021-12-10 14:04:11 +0200

    Task 9

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapPoints p1 = new SwapPoints(5, 2);
+            SwapPoints p2 = new SwapPoints(-3, 6);
+
+            Swapping(p1, p2);
+
+            Console.WriteLine("(" + p1._x + ", " + p1._y + ")");
+            Console.WriteLine("(" + p2._x + ", " + p2._y + ")");
+
+            Console.ReadKey();
+        }
+
+        public static void Swapping(SwapPoints x, SwapPoints y)
+        {
+            int tempXX = x._x;
+            int tempXY = x._y;
+            x._x = y._x;
+            x._y = y._y;
+            y._x = tempXX;
+            y._y = tempXY;
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise 9/SwapPoints.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    internal class SwapPoints
+    {
+        public int _x;
+        public int _y;
+        public SwapPoints _p1;
+        public SwapPoints _p2;
+
+        public SwapPoints(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
