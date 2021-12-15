2d33828c9181e3946d4993b494a305a72b5b9e82
KKuzmane <kristinekuzmane@gmail.com>
2021-12-15 16:22:42 +0200

    Task 4

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Collections/Exercise 4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int res = number;
+
+            while(res != 1)
+            {
+                int[] math = res.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+
+                res = 0;
+
+                foreach(int digit in math)
+                {
+                    res += Convert.ToInt32(Math.Pow(digit, 2));
+                }
+                if(res == number)
+                {
+                    Console.WriteLine(res);
+                    break;
+                }
+                Console.WriteLine(res);
+            }
+
+            if (res == 1)
+            {
+                Console.WriteLine(number + " is HAPPY number :) !");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a HAPPY number :( !");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
