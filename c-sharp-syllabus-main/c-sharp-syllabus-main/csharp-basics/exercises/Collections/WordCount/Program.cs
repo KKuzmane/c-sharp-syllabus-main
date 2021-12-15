132cfa84256b75cc066d6f50bc90f6903adec4f6
KKuzmane <kristinekuzmane@gmail.com>
2021-12-15 17:57:15 +0200

    Task 5

--- c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Collections/WordCount/Program.cs
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WordCount
@@ -10,7 +12,21 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int charCount;
+            int wordCount;
+            int lineCount;
+
+            string fileText = File.ReadAllText("../../lear.txt");
+
+            charCount = File.ReadAllLines("../../lear.txt").Sum(s => s.Length);
+            wordCount = Regex.Matches(fileText, @"\b\w+\b").Count;
+            lineCount = File.ReadLines("../../lear.txt").Count();
+
+            Console.WriteLine("Chars = " + charCount);
+            Console.WriteLine("Words = " + wordCount);
+            Console.WriteLine("Lines = " + lineCount);
+
+            Console.ReadKey();
         }
     }
 }
