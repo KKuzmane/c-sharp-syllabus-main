3750abc512a8919c9a25656c3f1cc3a41613fdce
KKuzmane <kristinekuzmane@gmail.com>
2022-01-03 15:06:50 +0200

    Task 4

---
+++ c-sharp-syllabus-main/c-sharp-syllabus-main/csharp-basics/exercises/Polymorphism/Firm/Commision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firm
+{
+    internal class Commision : Hourly
+    {
+        private double _totalSales;
+        private double _commissionRate;
+
+        public Commision(string name, string address, string phone, string socSecNumber, double rate, double commissionRate) : base(name, address, phone, socSecNumber, rate)
+        {
+            _totalSales = 0;
+            _commissionRate = commissionRate;
+        }
+
+        public void AddSales(double totalSales)
+        {
+            _totalSales = totalSales * _commissionRate;
+        }
+    }
+}
